42cdf414806f0723010da0fe4684e1ad3fe44e03
Abhishek Srivastava <abhishek.sr017@gmail.com>
2018-08-10 15:31:15 +0530

    Create post with relationship category

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\PostsCreateRequest;
 use App\Photo;
 use App\Post;
+use App\Category;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -31,7 +32,10 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::lists('name', 'id')->all();
+
+
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
